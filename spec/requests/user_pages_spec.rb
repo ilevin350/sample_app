d3b4d212a9d471cb221242c1accfeafd47a25338
Ira Levin <ilevin@bdata.com>
2013-12-26 09:08:21 -0800

    Finish Ch 7 user signup

--- spec/requests/user_pages_spec.rb
+++ spec/requests/user_pages_spec.rb
@@ -23,6 +23,7 @@ describe 'User pages' do
     before { visit signup_path }
 
     let(:submit) { "Create my account" }
+
     describe "with invalid information" do
       it "should not create a user" do
         expect { click_button submit }.not_to change(User, :count)
@@ -31,9 +32,9 @@ describe 'User pages' do
 
     describe "with valid information" do
       before do
-        fill_in "Name",         with: "Example User"
-        fill_in "Email",        with: "user@example.com"
-        fill_in "Password",     with: "foobar"
+        fill_in "Name", with: "Example User"
+        fill_in "Email", with: "user@example.com"
+        fill_in "Password", with: "foobar"
         fill_in "Confirmation", with: "foobar"
       end
 
